2ab1a6ed1ed61ae013c316e3c5872b48ccce0311
John <jfranti@gmail.com>
2017-07-19 08:56:05 -0700

    repair todolist functionality

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Objects;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/tasks")]
+        public ActionResult Tasks()
+        {
+            List<Task> allTasks = Task.GetAll();
+            return View(allTasks);
+        }
+
+        [HttpGet("/tasks/new")]
+        public ActionResult TaskForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/tasks")]
+        public ActionResult AddTask()
+        {
+            Task newTask = new Task(Request.Form["new-task"]);
+            List<Task> allTasks = Task.GetAll();
+            return View("Tasks", allTasks);
+        }
+
+        [HttpGet("/tasks/{id}")]
+        public ActionResult TaskDetail(int id)
+        {
+            Task task = Task.Find(id);
+            return View(task);
+        }
+
+    }
+}
