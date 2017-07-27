9e194fabb88188a0ea5d50981d32bab52c76947c
John <jfranti@gmail.com>
2017-07-27 11:23:05 -0700

    update some shizz

--- Models/Task.cs
+++ Models/Task.cs
@@ -2,48 +2,60 @@ using System.Collections.Generic;
 
 namespace ToDoList.Objects
 {
-  public class Task
-  {
-    private string _description;
-    private int _id;
-    private static List<Task> _instances = new List<Task> {};
+    public class Task
+    {
+        public static bool IsLeapYear(int year)
+        {
+          //code will go here
+          if (year % 400 == 0)
+          {
+            return true;
+          }
+          else if (year % 100 == 0)
+          {
+            return false;
+          }
+          else
+          {
+            return year % .4 == 0;
+          }
+        }
 
-    public Task(string description)
-    {
-      _description = description;
-      _instances.Add(this);
-      _id = _instances.Count;
-    }
+        private string _description;
+        private int _id;
+        private static List<Task> _instances = new List<Task> {};
 
-    public int GetNumber()
-    {
-      return 1;
-    }
+        public Task(string description)
+        {
+            _description = description;
+            _instances.Add(this);
+            _id = _instances.Count;
+        }
 
-    public string GetDescription()
-    {
-      return _description;
-    }
+        public string GetDescription()
+        {
+            return _description;
+        }
 
-    public void SetDescription(string newDescription)
-    {
-      _description = newDescription;
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+        public static List<Task> GetAll()
+        {
+            return _instances;
+        }
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+        public static Task Find(int searchId)
+        {
+            return _instances[searchId-1];
+        }
     }
-    public int GetId()
-    {
-      return _id;
-    }
-    public static List<Task> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static Task Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-  }
 }
--- Tests/TaskTest.cs
+++ Tests/TaskTest.cs
@@ -5,50 +5,49 @@ using System;
 namespace ToDoList.Objects
 {
     [TestClass]
-    public class ToDoListTest
-    // public class ToDoListTest : IDisposable
+    // public class ToDoListTest
+    public class ToDoListTest : IDisposable
     {
-        // public void Dispose()
-        // {
-        //   Task.ClearAll();
-        // }
-        // [TestMethod]
-        // public void GetDescription_ReturnsDescription_String()
-        // {
-        //   //Arrange
-        //   string description = "Walk the dog.";
-        //   Task newTask = new Task(description);
-        //
-        //   //Act
-        //   string result = newTask.GetDescription();
-        //
-        //   //Assert
-        //   Assert.AreEqual(description, result);
-        // }
-        //
-        // [TestMethod]
-        // public void GetAll_ReturnsTasks_TaskList()
-        // {
-        //   //Arrange
-        //   string description01 = "Walk the dog";
-        //   string description02 = "Wash the dishes";
-        //   Task newTask1 = new Task(description01);
-        //   Task newTask2 = new Task(description02);
-        //   List<Task> newList = new List<Task> { newTask1, newTask2 };
-        //
-        //   //Act
-        //   List<Task> result = Task.GetAll();
-        //   Console.WriteLine(result.Count);
-        //   Console.WriteLine(newList.Count);
-        //
-        //   //Assert
-        //   CollectionAssert.AreEqual(newList, result);
-        // }
-        [TestMethod]
-        public void GetNumber_ReturnsNumber_1()
+        public void Dispose()
         {
-          
+          Task.ClearAll();
         }
 
+        [TestMethod]
+        public void GetDescription_ReturnsDescription_String()
+        {
+          //Arrange
+          string description = "Walk the dog.";
+          Task newTask = new Task(description);
+
+          //Act
+          string result = newTask.GetDescription();
+
+          //Assert
+          Assert.AreEqual(description, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsTasks_TaskList()
+        {
+          //Arrange
+          string description01 = "Walk the dog";
+          string description02 = "Wash the dishes";
+          Task newTask1 = new Task(description01);
+          Task newTask2 = new Task(description02);
+          List<Task> newList = new List<Task> { newTask1, newTask2 };
+
+          //Act
+          List<Task> result = Task.GetAll();
+
+          //Assert
+          CollectionAssert.AreEqual(newList, result);
+        }
+
+        [TestMethod]
+        public void IsLeapYear_ForNumberDivisibleByFour_true()
+        {
+            Assert.AreEqual(true, Task.IsLeapYear(8));
+        }
     }
 }
